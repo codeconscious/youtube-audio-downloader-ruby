91f0ba3d1f06cfe7f54dffd971f666d2752a7880
CodeConscious <hello@codeconscio.us>
2022-06-28 20:58:27 +0900

    Properly read regex match text

--- audio-download.rb
+++ audio-download.rb
@@ -4,14 +4,15 @@ if ARGV.length == 0
 end
 
 # Download audio for each valid argument.
-ARGV.each do |arg|
-    if arg =~ /^[\w|-]{11}$|(?<=v=|v\\=)[\w|-]{11}|(?<=youtu\.be\/).{11}/
-        puts "■ Processing ID " + arg
-        $command = "yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 --split-chapters https://www.youtube.com/watch?v=#{arg}"
+ARGV.each do |rawArg|
+    id = rawArg[/^[\w|-]{11}$|(?<=v=|v\\=)[\w|-]{11}|(?<=youtu\.be\/).{11}/]
+    if id
+        puts "■ Processing ID " + id
+        $command = "yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 --split-chapters https://www.youtube.com/watch?v=#{id}"
         puts "Command: " + $command
         system $command # Runs the command, piping in its output too.
     else
-        puts "■ ERROR: #{arg} is not a valid source ID"
+        puts "■ ERROR: A valid ID could not be parsed from '#{rawArg}'"
     end
 end
 
