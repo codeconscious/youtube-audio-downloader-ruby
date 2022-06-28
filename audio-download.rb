c99017618a32063154767bddb4df0d7ed8281514
CodeConscious <hello@codeconscio.us>
2022-06-28 20:37:35 +0900

    Use regex to verify IDs

--- audio-download.rb
+++ audio-download.rb
@@ -3,16 +3,16 @@ if ARGV.length == 0
     return
 end
 
-# Download audio for each argument.
+# Download audio for each valid argument.
 ARGV.each do |arg|
-    if arg.length == 11
+    if arg =~ /^[\w|-]{11}$|(?<=v=)[\w|-]{11}|(?<=youtu\.be\/).{11}/
         puts "■ Processing ID " + arg
         $command = "yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 --split-chapters https://www.youtube.com/watch?v=#{arg}"
         puts "Command: " + $command
         system $command # Runs the command, piping in its output too.
     else
-        puts "■ ERROR: #{arg} is not a valid source ID."
+        puts "■ ERROR: #{arg} is not a valid source ID"
     end
 end
 
-puts "✅ Downloading complete."
+puts "Downloading complete"
