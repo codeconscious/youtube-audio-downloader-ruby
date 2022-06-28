d1d933cb30257f78567cebbdbce8bc153715100c
CodeConscious <hello@codeconscio.us>
2022-06-28 19:32:47 +0900

    Support args

--- audio-download.rb
+++ audio-download.rb
@@ -1,7 +1,18 @@
-$videoId = "5B1rB894B1U" # Tiny test video
+if ARGV.length == 0
+    puts "ERROR: No IDs supplied."
+    return
+end
 
-$command = "yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 --split-chapters https://www.youtube.com/watch?v=#{$videoId}"
-puts $command
-system $command # Runs the command, showing its output.
+# Download audio for each argument.
+ARGV.each do |a|
+    if a.length == 11
+        puts "■ Processing ID " + a
+        $command = "yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 --split-chapters https://www.youtube.com/watch?v=#{a}"
+        puts "Command: " + $command
+        system $command # Runs the command, piping in its output too.
+    else
+        puts "■ ERROR: #{a} is not a valid source ID."
+    end
+end
 
-puts "Download (hopefully) complete."
+puts "Downloads (hopefully) complete."
