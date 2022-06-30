4eabeaeab56c2d2337d928fe7f5ed470e7a44a86
CodeConscious <hello@codeconscio.us>
2022-06-30 12:13:23 +0900

    Rename variables to standard naming style

--- download.rb
+++ download.rb
@@ -4,29 +4,29 @@ if ARGV.length == 0
     return
 end
 
-successCount = 0
-failureCount = 0
+success_count = 0
+failure_count = 0
 
-# Download audio for each valid argument.
+# Download audio for each valid ID.
 ARGV.each do |rawArg|
     id = rawArg[/^[\w|-]{11}$|(?<=v=|v\\=)[\w|-]{11}|(?<=youtu\.be\/).{11}/]
     if id
         puts "■ Processing ID " + id
         $command = "yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 --split-chapters https://www.youtube.com/watch?v=#{id}"
         puts "Command: " + $command
-        wasSuccess = system $command # Runs the command, piping in its output too.
-        if wasSuccess == true
-            successCount += 1
+        was_success = system $command # Runs the command, piping in its output too.
+        if was_success == true
+            success_count += 1
         else
-            failureCount += 1
+            failure_count += 1
         end
     else
         puts "■ ERROR: A valid ID could not be parsed from '#{rawArg}'"
-        failureCount += 1
+        failure_count += 1
     end
 end
 
 # Print the results.
-successLabel = successCount == 1 ? "success" : "successes"
-failureLabel = failureCount == 1 ? "failure" : "failures"
-puts "Done with #{successCount} #{successLabel} and #{failureCount} #{failureLabel}"
+success_label = success_count == 1 ? "success" : "successes"
+failure_label = failure_count == 1 ? "failure" : "failures"
+puts "Done with #{success_count} #{success_label} and #{failure_count} #{failure_label}"
