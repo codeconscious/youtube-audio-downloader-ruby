79bbfcc04155daef04a702a2b0e6ffa0c6623f65
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2022-07-02 12:30:49 +0900

    Add file counts

--- move_files.rb
+++ move_files.rb
@@ -1,16 +1,22 @@
 require 'fileutils'
 
+# TODO: Add error handling for IO operations.
 def move(extension, target_directory_file)
     if File.exist?(target_directory_file)
         path = File.read(target_directory_file).chomp
         if Dir.exist?(path)
-            print "Will move all #{extension.upcase} files to path '#{path}'... "
+            file_count = Dir.glob('*.' + extension).count
+            if (file_count == 0)
+                puts "No #{extension.upcase} files to move were found. (Was there a download error?)"
+                return
+            end
+            print "Will move #{file_count} #{extension.upcase} files to path '#{path}'... "
             FileUtils.mv Dir.glob('*.' + extension), path
             puts "done!"
         else
-            puts "ERROR: Path '#{path}' not found, so no files moved."
+            puts "Path '#{path}' was not found, so no files were moved."
         end
     else
-        puts "No files moved because '#{target_directory_file}' was not found."
+        puts "No files were moved because the file '#{target_directory_file}' was not found."
     end
 end
